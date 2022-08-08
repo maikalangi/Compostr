fbb4d459c5324cf60bd19943744f8fcb35672b97
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-08-08 19:30:23 -0400

    adding routes

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,23 @@
+import React,{ useState,useEffect } from "react";
+
+const Calculator = () =>{
+
+    return (
+    <div className="Calculator">
+        <div className="CompostCalc">
+            <div className="CompPic">
+            STIGGG
+            </div>
+            <div className="CompAmount">
+                <div id="compSmall">SOME OTHER STUFF</div>
+                <div id="compMedium"></div>
+                <div id="compLarge"></div>
+            </div>
+
+        </div>
+    </div>
+    )
+
+}
+
+export default Calculator
