8e0eaee3526f0bb9f7b082af4915144990d5bdfd
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-08-09 11:34:31 -0400

    added shadows and last button

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,7 +2,6 @@ import React,{ useState,useEffect } from "react";
 import "../style/calc.css"
 
 const Calculator = () =>{
-    const [counts, setCounts] = useState([0,0,0])
     const [sCount, setSCount] = useState(0)
     const [mCount, setMCount] = useState(0)
     const [lCount, setLCount] = useState(0)
@@ -10,28 +9,42 @@ const Calculator = () =>{
 
     const incCount = (pm,val) =>{
 
+
+
+        if(val === "small"){
+            let nCount = sCount+pm
+            setSCount(nCount)
+            if(nCount<0){
+                setSCount(0)
+            }
+        }
         if(val==="med"){
             let nCount = mCount+pm
+
             setMCount(nCount)
+            if(nCount<0){
+                setMCount(0)
+            }
         }
         if(val==="large"){
             let nCount = lCount+pm
             setLCount(nCount)
+            if(nCount<0){
+                setLCount(0)
+            }
         }
-        if(val === "small"){
-            let nCount = sCount+pm
-            setSCount(nCount)
-        }
+        
     }
     
     const setNumber = (targ,value)=>{
-        console.log(typeof value)
-        console.log(value)
+
         let vInt = parseInt(value)
      
         // let nCounts = counts
         if(targ.name==="smallcount"){
             if(isNaN(value)|| value === ""){
+                setSCount(0)
+            }else if(vInt < 0){
                 setSCount(0)
             }else
             setSCount(vInt)
@@ -43,11 +56,12 @@ const Calculator = () =>{
             setMCount(vInt)
         }
         if(targ.name==="largecount"){
+            if(isNaN(value)|| value === ""){
+                setLCount(0)
+            }else
+            setLCount(vInt)
         }
     }
-    // useEffect(() => {
-        
-    //   },[counts]);
 
     return (
     <div className="Calculator">
@@ -56,7 +70,7 @@ const Calculator = () =>{
                 this is a picture
             </div>
             <div className="CompAmount">
-                <div className="compSmall">Small Counter<div className="counter">
+                <div className="compSmall"><div className="compText">Small bag (less than 3 gallons)</div><div className="counter">
                 <div className="minus"onClick={()=>incCount(-1,"small")}>-</div>
                 <input className="counterCount"
                 type="text"
@@ -64,14 +78,14 @@ const Calculator = () =>{
                 placeholder="0"
                 value={sCount}
                 onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"small")}>+</div></div></div>
-                <div className="compMedium">Medium Counter<div className="counter"><div className="minus"onClick={()=>incCount(-1,"med")}>-</div>
+                <div className="compMedium"><div className="compText">Full bag (3 gallons or more)</div><div className="counter"><div className="minus"onClick={()=>incCount(-1,"med")}>-</div>
                 <input className="counterCount"
                 type="text"
                 name="medcount"
                 placeholder="0"
                 value={mCount}
                 onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"med")}>+</div></div></div>
-                <div className="compLarge">Large Counter<div className="counter"><div className="minus"onClick={()=>incCount(-1,"large")}>-</div>
+                <div className="compLarge"><div className="compText">Large Bag</div><div className="counter"><div className="minus"onClick={()=>incCount(-1,"large")}>-</div>
                 <input className="counterCount"
                 type="text"
                 name="largecount"
@@ -79,7 +93,7 @@ const Calculator = () =>{
                 value={lCount}
                 onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"large")}>+</div></div></div>
             </div>
-
+        <div className="finishButt">Finish Composting</div>
         </div>
     </div>
     )
