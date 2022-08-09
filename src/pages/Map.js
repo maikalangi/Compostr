c9242b045dc1f9f247c31b66557b05817c80332e
pepermz <jose.ram.18@gmail.com>
2022-08-08 22:00:12 -0600

    added map

---
+++ src/pages/Map.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+
+function Home(){
+  
+  
+  // if(!isLoaded) return <div>Loading...</div>
+  return(    
+    <div className="App">
+      <iframe style={{ width:"100%", height:"600px"}} src="https://maps.google.com/maps?q=composter%20in%20miami&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Home
