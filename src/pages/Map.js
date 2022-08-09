710b757ef586885a987d5e53cd97d3328a3c6d5c
pepermz <jose.ram.18@gmail.com>
2022-08-08 22:39:40 -0600

    made the map mobile

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -9,7 +9,7 @@ function Home(){
   // if(!isLoaded) return <div>Loading...</div>
   return(    
     <div className="App">
-        <iframe style={{ width:"100%", height:"600px"}}  
+        <iframe style={{ width:"100%", height:"800px", cursor:"grab"}}  
           src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d229925.11535698533!2d-80.40449824573055!3d25.784435159782443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1scomposting%20in%20miami!5e0!3m2!1sen!2sus!4v1660018638848!5m2!1sen!2sus"
           width="600"
           height="450"  
