1afede798f49b9a22ac0ee4faa131f63be3fb125
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-08-09 09:34:59 -0400

    added counters and some style

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,6 +2,52 @@ import React,{ useState,useEffect } from "react";
 import "../style/calc.css"
 
 const Calculator = () =>{
+    const [counts, setCounts] = useState([0,0,0])
+    const [sCount, setSCount] = useState(0)
+    const [mCount, setMCount] = useState(0)
+    const [lCount, setLCount] = useState(0)
+
+
+    const incCount = (pm,val) =>{
+
+        if(val==="med"){
+            let nCount = mCount+pm
+            setMCount(nCount)
+        }
+        if(val==="large"){
+            let nCount = lCount+pm
+            setLCount(nCount)
+        }
+        if(val === "small"){
+            let nCount = sCount+pm
+            setSCount(nCount)
+        }
+    }
+    
+    const setNumber = (targ,value)=>{
+        console.log(typeof value)
+        console.log(value)
+        let vInt = parseInt(value)
+     
+        // let nCounts = counts
+        if(targ.name==="smallcount"){
+            if(isNaN(value)|| value === ""){
+                setSCount(0)
+            }else
+            setSCount(vInt)
+        }
+        if(targ.name==="medcount"){
+            if(isNaN(value)|| value === ""){
+                setMCount(0)
+            }else
+            setMCount(vInt)
+        }
+        if(targ.name==="largecount"){
+        }
+    }
+    // useEffect(() => {
+        
+    //   },[counts]);
 
     return (
     <div className="Calculator">
@@ -10,9 +56,28 @@ const Calculator = () =>{
                 this is a picture
             </div>
             <div className="CompAmount">
-                <div className="compSmall">Small Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
-                <div className="compMedium">Medium Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
-                <div className="compLarge">Large Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
+                <div className="compSmall">Small Counter<div className="counter">
+                <div className="minus"onClick={()=>incCount(-1,"small")}>-</div>
+                <input className="counterCount"
+                type="text"
+                name="smallcount"
+                placeholder="0"
+                value={sCount}
+                onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"small")}>+</div></div></div>
+                <div className="compMedium">Medium Counter<div className="counter"><div className="minus"onClick={()=>incCount(-1,"med")}>-</div>
+                <input className="counterCount"
+                type="text"
+                name="medcount"
+                placeholder="0"
+                value={mCount}
+                onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"med")}>+</div></div></div>
+                <div className="compLarge">Large Counter<div className="counter"><div className="minus"onClick={()=>incCount(-1,"large")}>-</div>
+                <input className="counterCount"
+                type="text"
+                name="largecount"
+                placeholder="0"
+                value={lCount}
+                onChange={(e)=>setNumber(e.target,e.target.value)}/><div className="plus" onClick={()=>incCount(1,"large")}>+</div></div></div>
             </div>
 
         </div>
