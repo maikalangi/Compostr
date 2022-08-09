f65303c5a9d128de06861d441bb1edb5f370723f
pepermz <jose.ram.18@gmail.com>
2022-08-08 22:21:44 -0600

    better map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -4,11 +4,19 @@ import React from "react";
 
 function Home(){
   
-  
+  let apiKey = process.env.YOUR_API_KEY
+  let url = `https://www.google.com/maps/embed/v1/MAP_MODE?key=${apiKey}`
   // if(!isLoaded) return <div>Loading...</div>
   return(    
     <div className="App">
-      <iframe style={{ width:"100%", height:"600px"}} src="https://maps.google.com/maps?q=composter%20in%20miami&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe style={{ width:"100%", height:"600px"}}  
+          src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d229925.11535698533!2d-80.40449824573055!3d25.784435159782443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1scomposting%20in%20miami!5e0!3m2!1sen!2sus!4v1660018638848!5m2!1sen!2sus"
+          width="600"
+          height="450"  
+          allowfullscreen="true" 
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
       <h1></h1>
     </div>
   )
