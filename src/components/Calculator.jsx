951cfea61bb0a07b0fddfa179cccac6228127235
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-08-08 22:42:15 -0400

    added some buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,5 @@
 import React,{ useState,useEffect } from "react";
+import "../style/calc.css"
 
 const Calculator = () =>{
 
@@ -6,12 +7,12 @@ const Calculator = () =>{
     <div className="Calculator">
         <div className="CompostCalc">
             <div className="CompPic">
-            STIGGG
+                this is a picture
             </div>
             <div className="CompAmount">
-                <div id="compSmall">SOME OTHER STUFF</div>
-                <div id="compMedium"></div>
-                <div id="compLarge"></div>
+                <div className="compSmall">Small Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
+                <div className="compMedium">Medium Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
+                <div className="compLarge">Large Counter<div className="counter"><div className="minus">-</div><div className="counterCount">0</div><div className="plus">+</div></div></div>
             </div>
 
         </div>
