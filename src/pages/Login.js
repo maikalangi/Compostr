cd0dc7b6f2ffc164c3e8c53bfb7f53859aa24a37
maikalangi <maikalangi@gmail.com>
2022-08-09 17:34:15 -0700

    button works

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,34 +4,34 @@ import apple from '../img/apple.png';
 import facebook from '../img/facebook.png';
 import twitter from '../img/twitter.png';
 import google from '../img/google.png';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () =>{
 
+    // const history = BrowserRouter();
+    // const map = () => history.push('/map');
+
     return (
     <div className="login">
-        <img src={logo} alt="logo" />
-        <br />
-        compostr
-        <br />
-        Making your city greener one bag at a time
-        <br />
-        <input type="email" placeholder="example@gmail.com"/>
-        <br />
-        <input type="text" />
-        <br />
-        Forgot Password?
-        <br />
-        <button>Log In</button>
-        <br />
-        <button>Sign Up</button>
-        <br />
-        or
-        <br />
-        <img src={apple} alt="apple" /> 
-        <img src={facebook} alt="facebook" />
-        <img src={twitter} alt="twitter" />
-        <img src={google} alt="google" />
-
+        <div className='card'>
+            <img className='logo' src={logo} alt="logo" />
+            <p>compostr</p>
+            Making your city greener one bag at a time
+            <input className='email' type="email" placeholder="example@gmail.com"/>
+            <input className='password' type="text" />
+            <p>Forgot Password?</p>
+            <Link to="/main">
+                <button>Log In</button>
+            </Link>
+            <button>Sign Up</button>
+            <p>or</p>
+            <div className='signups'>
+                <img src={apple} alt="apple" /> 
+                <img src={facebook} alt="facebook" />
+                <img src={twitter} alt="twitter" />
+                <img src={google} alt="google" />
+            </div>
+        </div>
     </div>
     )
 
